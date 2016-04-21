a47a772067f432efaa1e09e2ecbfd71eae082578
Andreas <andreas@fractal-labs.com>
2016-04-21 17:42:14 +0200

    cleanup

--- src/main/java/com/asigner/elizabot/eliza/ElizaApp.java
+++ src/main/java/com/asigner/elizabot/eliza/ElizaApp.java
@@ -1,40 +1,28 @@
 package com.asigner.elizabot.eliza;
 
-import java.awt.*;
+import java.io.DataInputStream;
+import java.io.IOException;
 
 /**
  *  Eliza Application.
  */
 public class ElizaApp {
 
-    static String scriptPathname = "script";
-    static String testPathname = "test";
-    static String scriptURL = "http://www.monmouth.com/~chayden/eliza/script";
-    //static String scriptURL = "http://www-gbcs.mt.att.com/~cch/eliza/script";
-
-    static final boolean useWindow = false;
-    static final boolean local = true;
-
     public static void main(String args[]) {
-        ElizaMain eliza = new ElizaMain();
-        String script = scriptPathname;
-        String test = testPathname;
-        if (! local) script = scriptURL;
-        if (args.length > 0) script = args[0];
-        if (args.length > 1) test = args[1];
-        int res = eliza.readScript(local, script);
-        if (res != 0) System.exit(res);
-        if (useWindow) {
-            Frame f = new Frame("Eliza");
-            f.resize(100, 100);
-            f.show();
-            Panel p = new Panel();
-            f.add(p);
-            p.show();
-            res = eliza.runProgram(p);
-        } else {
-            res = eliza.runProgram(null);
-            System.exit(res);
+        try {
+            Eliza eliza = new Eliza();
+            DataInputStream in = new DataInputStream(System.in);
+            String s = "Hello.";
+            while (true) {
+                System.out.println(">> " + s);
+                String reply = eliza.processInput(s);
+                System.out.println(reply);
+                if (eliza.isFinished()) break;
+                s = in.readLine();
+                if (s == null) break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
